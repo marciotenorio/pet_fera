641ab0ae2ca3396bf98fe588f8c7eeaa3b010863
Márcio Tenório Júnior <marciotenorio@A305-14L.imd.local>
2019-05-07 20:41:51 -0300

    classes em andamento

---
+++ classes/tratador.h
@@ -0,0 +1,15 @@
+class Tratador: public Funcionario
+{
+	int nivel_seguranca;
+
+public:
+	
+	Tratador(unsigned int c_id,
+	string c_nome,
+	string c_cpf,
+	short c_idade,
+	string c_tipo_sanguineo,
+	char c_fator_rh,
+	string c_especialidade)d
+
+}
