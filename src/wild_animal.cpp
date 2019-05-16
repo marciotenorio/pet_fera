90d82510ba91d190f903c36efec963fea1b75244
Márcio Tenório <marciotenorio@ufrn.edu.br>
2019-05-16 17:50:12 -0300

    In progress

--- src/wild_animal.cpp
+++ src/wild_animal.cpp
@@ -1,6 +1,29 @@
-#include "include/wild_animal.h"
+#include "wild_animal.h"
 
-WildAnimal::WildAnimal(string c_ibama_authorization)
-{
-    ibama_authorization = c_ibama_authorization;
+WildAnimal(
+    string ibama_authorization
+):NativeAnimal(
+    uf_origin,
+    authorization
+):ExoticAnimal(
+    country_origin
+){
+    this->ibama_authorization = ibama_authorization;
 }
+
+//~WildAnimal();
+
+string get_ibama_authorization(){
+    return ibama_authorization;
+}
+
+void set_ibama_authorization(string ibama_authorization){
+    this->ibama_authorization = ibama_authorization;
+}
+
+//How to implement this
+friend istream &operator>>(istream &input, WildAnimal &wild_animal){
+    cout<< "Insira os dados do animal silvestre: "<<endl;
+    input >> static_cast<NativeAnimal &, ExoticAnimal &>()
+}
+friend ostream &operator<<(ostream &output, const WildAnimal &wild_animal);
